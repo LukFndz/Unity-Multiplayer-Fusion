bf99c998788c4f8041582fe6ab5265415a7e5abf
Luca Fernandez <56230255+LukFndz@users.noreply.github.com>
2023-04-11 22:14:07 -0300

    Throw

---
+++ Fish Catcher/Assets/Scripts/PlayerThrow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerThrow : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    private bool _isFishing;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1) && !_isFishing)
+        {
+            _animator.Play("Throw");
+            _isFishing = true;
+        }
+
+    }
+}
